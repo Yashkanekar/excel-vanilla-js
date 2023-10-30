f5dd3d4c09bc768a2bb3eef8030ebfae4f9d4486
Yashkanekar <kanekaryash.11@gmail.com>
2023-10-30 23:54:33 +0530

    completed cycle detection in formula functionality

--- cycleValidation.js
+++ cycleValidation.js
@@ -1,6 +1,6 @@
 // structure ->
 // [
-//       1  2  3  4  ...... 26 
+//       1  2  3  4  ...... 26
 //     [ [],[],[],[],...... ,[]] 1
 //     [ [],[],[],[],...... ,[]] 2
 //     [ [],[],[],[],...... ,[]] 3
@@ -11,12 +11,97 @@
 // ]
 
 // storage 2D matrix
-let graphComponentMatrix = []
+let graphComponentMatrix = [];
 
-for(let i = 0; i < row; i++){
-    let row = [] // 100 rows
-    for(let j = 0; j < col; j++){
-        row.push([]) // push 26 arrays in each of the 100 rows which represents a cell
+for (let i = 0; i < row; i++) {
+  let row = []; // 100 rows
+  for (let j = 0; j < col; j++) {
+    row.push([]); // push 26 arrays in each of the 100 rows which represents a cell
+  }
+  graphComponentMatrix.push(row); // push each of the 100 with 26 arrays (representing cells) inside the outer main array
+}
+
+function isGraphCyclic(graphComponentMatrix) {
+  let visited = []; // Node(cell) visit trace
+  let dfsVisited = []; // stack visit trace
+
+  for (let i = 0; i < row; i++) {
+    let visitedRow = [];
+    let dfsVisitedRow = [];
+    // initializing all the default visited values of all the cells in visitedRow and dfsVisitedRow as false
+    for (let j = 0; j < col; j++) {
+      visitedRow.push(false);
+      dfsVisitedRow.push(false);
     }
-    graphComponentMatrix.push(row) // push each of the 100 with 26 arrays (representing cells) inside the outer main array
+    visited.push(visitedRow);
+    dfsVisited.push(dfsVisitedRow);
+  }
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (visited[i][j] == false) {
+        let response = dfsCycleDetection(
+          graphComponentMatrix,
+          i,
+          j,
+          visited,
+          dfsVisited
+        ); // this function will check for the cycle in dependency of each and every cell and then its children and so on
+
+        if (response == true) return true;
+      }
+    }
+  }
+  return false;
+}
+
+// Start condition -> make visited and dfsVisited true
+// exit condition -> make dfsVisited false
+// if vis[i][j] is true then already visited no need to visit again
+// if vis[i][j] is true and dfsVisited[i][j] == true ; then there is a cycle in formula
+function dfsCycleDetection(
+  graphComponentMatrix,
+  srcr,
+  srcc,
+  visited,
+  dfsVisited
+) {
+  //first performing start conditions make visited[i][j] as true and dfsVisited[i][j] as true
+
+  visited[srcr][srcc] = true;
+  dfsVisited[srcr][srcc] = true;
+
+  // A1 -> [ [0,1],[1,0],[5,10],[4,9]... ]
+  //now looping over every children of the particular cell which is passed into the function
+  for (
+    let children = 0;
+    children < graphComponentMatrix[srcr][srcc].length;
+    children++
+  ) {
+    let [neighbourRow, neighbourCol] =
+      graphComponentMatrix[srcr][srcc][children]; // getting the row no and col no of each dependency inside of the child
+
+    // this condition means that the node hasnt been visited so we need to visit it and its children so if this condition is true then run the cycleDetection function for its inner children
+    if (visited[neighbourRow][neighbourCol] === false) {
+      let response = dfsCycleDetection(
+        graphComponentMatrix,
+        neighbourRow,
+        neighbourCol,
+        visited,
+        dfsVisited
+      );
+      if (response == true) return true; // found cycle so no need to explore more
+    } else if (
+      visited[neighbourRow][neighbourCol] === true &&
+      dfsVisited[neighbourRow][neighbourCol] === true
+    ) {
+      //found cycle returnn true
+      return true;
+    }
+  }
+
+  // performing end condition dfsVisited[srcr][srcc] as false
+  dfsVisited[srcr][srcc] = false;
+
+  return false; // no condition met so no cycle found hence code reaches this line and here we return false as no cycle is found
 }
--- formula.js
+++ formula.js
@@ -1,126 +1,148 @@
 //setting a value property in the sheetDB for every cell after the user focuses out of that cell
-for(let i = 0; i < row; i++){
-    for(let j = 0; j < col; j++){
-        let cell = document.querySelector(`.cell[rid = "${i}"][cid = "${j}"]`)
-        cell.addEventListener("blur", (e)=>{
-            let address = addressBar.value
-            let [activecell, cellProp] = getCellAndCellProp(address)
-            let enteredData = activecell.innerText
+for (let i = 0; i < row; i++) {
+  for (let j = 0; j < col; j++) {
+    let cell = document.querySelector(`.cell[rid = "${i}"][cid = "${j}"]`);
+    cell.addEventListener("blur", (e) => {
+      let address = addressBar.value;
+      let [activecell, cellProp] = getCellAndCellProp(address);
+      let enteredData = activecell.innerText;
 
-            if ( enteredData === cellProp.value ) return
-            // if data modifies remove P - C relation, formula empty, update children with new harcoded(modified) value
-            cellProp.value = enteredData
-            removeChildFromParent(cellProp.formula)
-            cellProp.formula = ""
-            updateChildrenCells(address)
-            // console.log(cellProp);
-        })
-    }
+      if (enteredData === cellProp.value) return;
+      // if data modifies remove P - C relation, formula empty, update children with new harcoded(modified) value
+      cellProp.value = enteredData;
+      removeChildFromParent(cellProp.formula);
+      cellProp.formula = "";
+      updateChildrenCells(address);
+      // console.log(cellProp);
+    });
+  }
 }
 
-let formulaBar = document.querySelector(".formula-bar")
-formulaBar.addEventListener("keydown", (e)=> {
+let formulaBar = document.querySelector(".formula-bar");
+formulaBar.addEventListener("keydown", (e) => {
+  let inputFormula = formulaBar.value;
+  if (e.key === "Enter" && inputFormula) {
+    //If change in formula then break the old P-C relationship, evaluate new formula, add new P-C relnship
+    let address = addressBar.value;
+    let [cell, cellProp] = getCellAndCellProp(address);
+    if (inputFormula !== cellProp.address)
+      removeChildFromParent(cellProp.formula);
 
-    let inputFormula = formulaBar.value
-    if (e.key === "Enter" && inputFormula){
-
-        //If change in formula then break the old P-C relationship, evaluate new formula, add new P-C relnship
-        let address = addressBar.value
-        let [cell, cellProp] = getCellAndCellProp(address)
-        if(inputFormula !== cellProp.address) removeChildFromParent(cellProp.formula)
-
-        let evaluatedValue = evaluateFormula(inputFormula)
-
-        addChildToGraphComponent(inputFormula, address)
-
-        //call the function to set evaluated value inside the cell and the input formula inside the formula bar
-        setCellUIandCellProp(evaluatedValue, inputFormula, address)
-        addChildToParent(inputFormula)
-        updateChildrenCells(address)
-        console.log(sheetDB);
+    
+    addChildToGraphComponent(inputFormula, address);
+    
+    //checking for cycles in formula
+    let isCyclic = isGraphCyclic(graphComponentMatrix);
+    if (isCyclic === true) {
+        alert("Your formula is cyclic");
+        removeChildFromGraphComponent(inputFormula, address) // if we find a cycle then need to remove the p->c relationship
+        return; //returning coz we dont wanna execute the following lines as there is a cycle in the formula
     }
-})
+    
+    let evaluatedValue = evaluateFormula(inputFormula);
 
-function addChildToGraphComponent(formula, childAddress){
-    let [crid, ccid] = decodeRIDCIDFromAddress(childAddress)
+    //call the function to set evaluated value inside the cell and the input formula inside the formula bar
+    setCellUIandCellProp(evaluatedValue, inputFormula, address);
+    addChildToParent(inputFormula);
+    updateChildrenCells(address);
+    // console.log(sheetDB);
+  }
+});
 
-    let encodedFormula = formula.split(" ")
-    for(let i = 0; i < encodedFormula.length; i++){
-        let asciiValue = encodedFormula[i].charCodeAt(0)
-        if(asciiValue >= 65 && asciiValue <= 90){
-            let [prid, pcid] = decodeRIDCIDFromAddress(encodedFormula(i))
-            graphComponentMatrix[prid][pcid].push([crid,ccid]) // push the children(dependent cells) into the parents graphcomponent address
-        }
+function addChildToGraphComponent(formula, childAddress) {
+  let [crid, ccid] = decodeRIDCIDFromAddress(childAddress);
+
+  let encodedFormula = formula.split(" ");
+  for (let i = 0; i < encodedFormula.length; i++) {
+    let asciiValue = encodedFormula[i].charCodeAt(0);
+    if (asciiValue >= 65 && asciiValue <= 90) {
+      let [prid, pcid] = decodeRIDCIDFromAddress(encodedFormula[i]);
+    //   console.log(prid, crid);
+      graphComponentMatrix[prid][pcid].push([crid, ccid]); // push the children(dependent cells) into the parents graphcomponent address
     }
-}
-
-// function to update all the children of the parent cell if the parent cell formula changes.
-function updateChildrenCells(parentAddress) {
-
-  let [parentCell, parentCellProp] = getCellAndCellProp(parentAddress)
-  let children = parentCellProp.children
-
-  // looping over each and every child in the parent's children array
-  for(let i = 0; i < children.length; i++){
-    let childAddress = children[i]
-    let [childCell, childCellProp] = getCellAndCellProp(childAddress)
-    let childFormula = childCellProp.formula 
-
-    let evaluatedValue = evaluateFormula(childFormula)
-    setCellUIandCellProp(evaluatedValue, childFormula, childAddress)
-    updateChildrenCells(childAddress); // recursively calling the function until all the children of childrens are updated 
   }
 }
 
 
-function addChildToParent(formula){
-    let encodedFormula = formula.split(" ")
-    let childAddress = addressBar.value //as the current selected cell has its formula dependent on other cells, it is the child and the dependent cells is the parent.
 
-    for(let i = 0; i < encodedFormula.length; i++){
-        let asciiValue = encodedFormula[i].charCodeAt(0);
-        if(asciiValue >= 65 && asciiValue <= 90){
-            let [parentCell, parentCellProp] = getCellAndCellProp(encodedFormula[i])
-            parentCellProp.children.push(childAddress)
-        }
+function removeChildFromGraphComponent(formula, childAddress) {
+  let [crid, ccid] = decodeRIDCIDFromAddress(childAddress);
+
+  let encodedFormula = formula.split(" ");
+  for (let i = 0; i < encodedFormula.length; i++) {
+    let asciiValue = encodedFormula[i].charCodeAt(0);
+    if (asciiValue >= 65 && asciiValue <= 90) {
+      let [prid, pcid] = decodeRIDCIDFromAddress(encodedFormula[i]);
+      graphComponentMatrix[prid][pcid].pop(); // remove the children(dependent cells) into the parents graphcomponent address
     }
+  }
 }
 
-function removeChildFromParent(formula){
-    let encodedFormula = formula.split(" ")
-    let childAddress = addressBar.value //as the current selected cell has its formula dependent on other cells, it is the child and the dependent cells is the parent.
+// function to update all the children of the parent cell if the parent cell formula changes.
+function updateChildrenCells(parentAddress) {
+  let [parentCell, parentCellProp] = getCellAndCellProp(parentAddress);
+  let children = parentCellProp.children;
 
-    for(let i = 0; i < encodedFormula.length; i++){
-        let asciiValue = encodedFormula[i].charCodeAt(0);
-        if(asciiValue >= 65 && asciiValue <= 90){
-            let [parentCell, parentCellProp] = getCellAndCellProp(encodedFormula[i])
-            let indx = parentCellProp.children.indexOf(childAddress)
-            parentCellProp.children.splice(indx,1) // remove the child inde from the parent cell prop's children array.
-        }
-    }
+  // looping over each and every child in the parent's children array
+  for (let i = 0; i < children.length; i++) {
+    let childAddress = children[i];
+    let [childCell, childCellProp] = getCellAndCellProp(childAddress);
+    let childFormula = childCellProp.formula;
+
+    let evaluatedValue = evaluateFormula(childFormula);
+    setCellUIandCellProp(evaluatedValue, childFormula, childAddress);
+    updateChildrenCells(childAddress); // recursively calling the function until all the children of childrens are updated
+  }
 }
 
-function evaluateFormula(formula){
+function addChildToParent(formula) {
+  let encodedFormula = formula.split(" ");
+  let childAddress = addressBar.value; //as the current selected cell has its formula dependent on other cells, it is the child and the dependent cells is the parent.
 
-    let encodedFormula = formula.split(" ") //splits the formula string into an array on the basis of empty space (" ")
-    for(let i =0; i < encodedFormula.length; i++){
-        let asciiValue = encodedFormula[i].charCodeAt(0); // get the ascii value of every element of the splitted formula string's array at the 0th index of the string
-        if(asciiValue >= 65 && asciiValue <= 90){  //check to see if the first element of the string is a character value
-            let [cell,cellProp] = getCellAndCellProp(encodedFormula[i])
-            encodedFormula[i] = cellProp.value //switch the value of the character to its actual stored value in the cell from the cellProp.value object]
-        }
+  for (let i = 0; i < encodedFormula.length; i++) {
+    let asciiValue = encodedFormula[i].charCodeAt(0);
+    if (asciiValue >= 65 && asciiValue <= 90) {
+      let [parentCell, parentCellProp] = getCellAndCellProp(encodedFormula[i]);
+      parentCellProp.children.push(childAddress);
     }
-    let decodedFormula = encodedFormula.join(" ")
-    return eval(decodedFormula)
+  }
 }
 
-function setCellUIandCellProp(evaluatedValue, formula, address){
-    let [cell, cellProp] = getCellAndCellProp(address)
+function removeChildFromParent(formula) {
+  let encodedFormula = formula.split(" ");
+  let childAddress = addressBar.value; //as the current selected cell has its formula dependent on other cells, it is the child and the dependent cells is the parent.
 
-    //set cell UI
-    cell.innerText = evaluateFormula(formula)
+  for (let i = 0; i < encodedFormula.length; i++) {
+    let asciiValue = encodedFormula[i].charCodeAt(0);
+    if (asciiValue >= 65 && asciiValue <= 90) {
+      let [parentCell, parentCellProp] = getCellAndCellProp(encodedFormula[i]);
+      let indx = parentCellProp.children.indexOf(childAddress);
+      parentCellProp.children.splice(indx, 1); // remove the child inde from the parent cell prop's children array.
+    }
+  }
+}
 
-    //set data in sheetDB
-    cellProp.formula = formula
-    cellProp.value = evaluatedValue
+function evaluateFormula(formula) {
+  let encodedFormula = formula.split(" "); //splits the formula string into an array on the basis of empty space (" ")
+  for (let i = 0; i < encodedFormula.length; i++) {
+    let asciiValue = encodedFormula[i].charCodeAt(0); // get the ascii value of every element of the splitted formula string's array at the 0th index of the string
+    if (asciiValue >= 65 && asciiValue <= 90) {
+      //check to see if the first element of the string is a character value
+      let [cell, cellProp] = getCellAndCellProp(encodedFormula[i]);
+      encodedFormula[i] = cellProp.value; //switch the value of the character to its actual stored value in the cell from the cellProp.value object]
+    }
+  }
+  let decodedFormula = encodedFormula.join(" ");
+  return eval(decodedFormula);
+}
+
+function setCellUIandCellProp(evaluatedValue, formula, address) {
+  let [cell, cellProp] = getCellAndCellProp(address);
+
+  //set cell UI
+  cell.innerText = evaluateFormula(formula);
+
+  //set data in sheetDB
+  cellProp.formula = formula;
+  cellProp.value = evaluatedValue;
 }
